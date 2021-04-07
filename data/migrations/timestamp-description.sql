e8a6852337b2fcb4219b42293adaaec60356ad46
Faisal Abuzaid <faisalabuzaid@outlook.com>
2021-04-07 23:19:04 +0300

    finishlab14

--- data/migrations/timestamp-description.sql
+++ data/migrations/timestamp-description.sql
@@ -4,7 +4,9 @@ CREATE TABLE authors (id SERIAL PRIMARY KEY, name VARCHAR(255));
 INSERT INTO authors(name) SELECT DISTINCT author FROM books;
 -- Add a new column into the old table "books", for the ids.
 ALTER TABLE books ADD COLUMN author_id INT;
--- retrieve the primary key on
+-- Retrieves the primary key on each authors and fills in the author id field in the books table
 UPDATE books SET author_id=author.id FROM (SELECT * FROM authors) AS author WHERE books.author = author.name;
+--  drop the author coloumn from the books table, as we replace it with id.
 ALTER TABLE books DROP COLUMN author;
+-- Set the author_id as the foreign key of the books table 
 ALTER TABLE books ADD CONSTRAINT fk_authors FOREIGN KEY (author_id) REFERENCES authors(id);
